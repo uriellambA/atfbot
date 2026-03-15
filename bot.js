d9e930b70216e0c73259e280896acb3ffa61a114
uriel <uriellam07@gmail.com>
2026-03-15 18:34:12 -0300

    fix

--- bot.js
+++ bot.js
@@ -18,7 +18,6 @@ const client = new Client({
 // Archivo de base de datos
 const DATABASE_FILE = 'players_data.json';
 
-const APPEALS_GUILD_ID      = process.env.APPEALS_GUILD_ID     || '1477484621666189405';
 const STAFF_ALERT_CHANNEL_ID = '1414386380682821646';
 const APPEALS_ROLE_APELANTE  = process.env.APPEALS_ROLE_APELANTE; // rol que ve solo #esperando
 const APPEALS_ROLE_MIEMBRO   = process.env.APPEALS_ROLE_MIEMBRO;  // rol que ve todo
